0dac6b3ca3df923ccb6afcf5d85264c604f7db1b
chaasfr <chaasfr@gmail.com>
2025-02-23 14:57:55 +0100

    book path is now an arg

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from stats import get_num_words
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("usage python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -14,7 +19,7 @@ def main():
     for item in chars_sorted_list:
         if not item["char"].isalpha():
             continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
+        print(f"{item['char']}: {item['num']}")
 
     print("--- End report ---")
 
